574377641220bc99078a85adbd008bd5742b1745
Frank <wanyoike39@gmail.com>
2023-07-03 23:46:46 +0300

    advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,16 @@
 # installs nginx server with custom http header
 exec { 'update':
     command => 'sudo apt-get -y update',
-    path => '/usr/bin',
-    before => Exec ['install Nginx'],    
 }
-
-exec { 'install Nginx':
-    command => 'sudo apt-get -y install nginx',
-    path => '/usr/bin',
-    before => Exec['add_header'],
+-> package {'nginx':
+    ensure => 'present',
 }
-
-exec { 'add_header':
-    environment => ["HOSTNAME=$hostname"],
-    command => 'sudo sed -i "s/http {/http {\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf',
-    path => '/usr/bin',
-    before => Exec['restart Nginx],
+-> file_line { 'http_header':
+    path  => '/etc/nginx/nginx.conf',
+    line  => "http {\n\tadd_header X-Served-By \"$HOSTNAME\";/",
+    match => 'http {',
 }
-
-exec {'restart Nginx':
-    command => 'sudo service nginx restart',
-    path => '/usr/bin',
+-> exec {'start':
+    command => 'sudo service nginx start'
+    path    => '/usr/sbin'
 }
