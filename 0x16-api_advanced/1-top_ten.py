44dbb82eea253edfa3c2e64e2c891d5d932aa89e
Frank <wanyoike39@gmail.com>
2023-08-08 22:39:26 +0300

    task 1 - fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,8 @@ import requests
 
 
 def top_ten(subreddit):
+    """function that queries the Reddit API and prints the titles of first 10
+    hot posts listed for a given subreddit"""
     header = {"User-Agent": "Mozilla/5.0 (X11;\
               Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"}
     url = "https://www.reddit.com/dev/api/r/{}/hot.json".format(subreddit)
