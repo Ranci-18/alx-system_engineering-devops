fbc42de27785fb189de514a218a08a3d994229e2
Frank <wanyoike39@gmail.com>
2023-08-08 22:25:37 +0300

    task 1 - fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,10 +5,10 @@ import requests
 
 
 def top_ten(subreddit):
-    header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0)\
-    Gecko/20100101 Firefox/115.0"}
-    url = "https://www.reddit.com/dev/api/\
-    r/{}/hot.json?limit=10".format(subreddit)
+    header = {"User-Agent": "Mozilla/5.0 (X11;\
+              Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"}
+    url = "https://www.reddit.com/dev/api/r/\
+    {}/hot.json?limit=10".format(subreddit)
 
     response = requests.get(url, headers=header)
     if response.status_code == 200:
